0e6e0653dcb04d2d793ed3f2b6b1182ffe725d54
Sumit Kumar <itzsumitkumarofficial@gmail.com>
2025-06-08 08:37:41 +0530

    playback added

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,23 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import DetailsPage from "./pages/DetailsPage";
 import Navbar from "./components/Navbar";
+import Playback from "./components/Playback";
 
 const App = () => {
   return (
     <>
-    <div className="fixed bottom-0 py-3 px-5 bg-black/50 w-full z-50">
-    <Navbar/>
-    </div>
+      <div className="fixed bottom-0 w-full z-50">
+        <div className="w-full px-2">
+          <Playback />
+        </div>
+        <div className="bg-black/50 backdrop-blur-lg w-full px-5 py-2">
+          <Navbar />
+        </div>
+      </div>
+
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/album/:id" element={<DetailsPage/>}/>
+        <Route path="/album/:id" element={<DetailsPage />} />
       </Routes>
     </>
   );
---
+++ src/components/Playback.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { FaPlay } from "react-icons/fa";
+import { MdSkipNext } from "react-icons/md";
+import { MdSkipPrevious } from "react-icons/md";
+import { useLocation } from "react-router-dom";
+
+const Playback = () => {
+    const location = useLocation();
+    const {title,coverImage, singer, color} = location.state || {};
+  return (
+    <div className="w-full rounded-xl py-2 px-2 backdrop-blur-sm flex justify-between items-center" style={{backgroundColor:color}}>
+      <div className="flex gap-3 justify-center items-center">
+        <div className="w-[45px] h-[45px] rounded-lg bg-red-500 overflow-hidden flex justify-center items-center bg-cover">
+            <img src={coverImage} alt="" />
+        </div>
+        <div className="flex justify-center items-start flex-col">
+          <p className="font-semibold text-[12px] text-white ">{title ? title : "Untitled"}</p>
+          <p className="text-[10px]">{singer ? singer:"untitled"}</p>
+        </div>
+      </div>
+      <div className="flex mr-2 justify-center items-center gap-3">
+        <MdSkipPrevious className="text-white text-4xl" />
+        <FaPlay className="text-white text-xl" />
+        <MdSkipNext className="text-white text-4xl" />
+      </div>
+    </div>
+  );
+};
+
+export default Playback;
